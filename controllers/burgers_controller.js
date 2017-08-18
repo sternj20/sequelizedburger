32eb3a6e8ef2c329989811dac159692b02b79a01
sternj20 <jessesternmusic@gmail.com>
2017-08-17 23:21:04 -0700

    full functionality in sequalize

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,20 +7,20 @@ router.get("/", function(req,res){
   var hbsObject;
   db.Burger.findAll({})
   .then(function(data){
-		hbsObject = {
-			burgers: data
-		};
+    hbsObject = {
+     burgers: data
+   };
     // burger.devoured(function(data){
     // hbsObject.devouredBurgers=data;
     console.log(hbsObject);
     res.render("index", hbsObject);
   // });
-	});
+});
 });
 
 router.post("/", function(req, res) {
   db.Burger.create({
-    name: req.body.burger_name,
+    burger_name: req.body.burger_name
   })
   .then(function(data){
     res.redirect("/");
@@ -29,13 +29,16 @@ router.post("/", function(req, res) {
 
 
 router.put("/:id", function(req, res) {
-  var condition = req.params.id;
-  console.log("condition", condition);
-
-  burger.update(condition, function() {
+  db.Burger.update(
+    { devoured:true},{where:
+     {id : req.params.id }
+    }
+  ).then(function(result) {
     res.redirect("/");
   });
 });
+
+
 
 // Export routes for server.js to use.
 module.exports = router;
